a5134c35c34b9ce05483c78e6b149026ef003ed1
qinnian <3582807388@qq.com>
2020-03-04 20:32:52 +0800

    Unity

---
+++ Game Design & Development/Code/Unity3d-Toggle/Assets/Tog01.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tog01 : MonoBehaviour {
+
+	Toggle mytog;
+
+	// Use this for initialization
+	void Start () {
+
+		mytog = transform.GetComponent<Toggle> ();
+		mytog.onValueChanged.AddListener (ToggleSelected);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ToggleSelected(bool Sel){
+
+		if (Sel) {
+
+			Debug.Log (transform.GetComponentInChildren<Text>().text + "is selected!");
+
+		}
+
+	}
+			
+}
---
+++ Game Design & Development/Code/Unity3d-Toggle/Assets/drop01.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class drop01 : MonoBehaviour {
+
+	Dropdown mydd;
+
+	// Use this for initialization
+	void Start () {
+
+		mydd = transform.GetComponent<Dropdown> ();
+
+		mydd.options.Clear ();
+
+		Dropdown.OptionData data1 = new Dropdown.OptionData ();
+		data1.text = "选项1"；
+
+		Dropdown.OptionData data1 = new Dropdown.OptionData ();
+		data1.text = "选项2"；
+
+
+		mydd.options.Add（data1）;
+		mydd.options.Add（data2）;
+
+		mydd.onValueChanged.AddListener (dropdownlistsel);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void dropdownlistsel(int index){
+		Debug.Log ("Index = " + mydd.value.ToString () + "选择内容：" + mydd.options [mydd.value].text);
+}
